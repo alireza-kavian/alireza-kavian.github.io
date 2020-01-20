eeb6aee97f9cad603b456bd6eee3a8bd90d74866
alireza kavian <kay.alireza@gmail.com>
2020-01-20 19:21:03 +0330

    version 4 started !

---
+++ precache-manifest.a0f5913b3e503952708c4b758d5a9a9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d36bf44d4770811701784a0a986d860",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6d14c207a1559a984378",
+    "url": "./static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "9386023bc3bec2fdec59",
+    "url": "./static/css/main.890642d8.chunk.css"
+  },
+  {
+    "revision": "6d14c207a1559a984378",
+    "url": "./static/js/2.2b00c3ea.chunk.js"
+  },
+  {
+    "revision": "ab06775db0006a1715066c989bcfff13",
+    "url": "./static/js/2.2b00c3ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9386023bc3bec2fdec59",
+    "url": "./static/js/main.ec758fa3.chunk.js"
+  },
+  {
+    "revision": "343dd621b700c08cccae",
+    "url": "./static/js/runtime-main.32c8be1e.js"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "./static/media/Roboto.df7b648c.ttf"
+  }
+]);
