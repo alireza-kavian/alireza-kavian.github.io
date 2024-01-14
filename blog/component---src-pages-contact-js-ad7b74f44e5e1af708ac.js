a7bef0ce2b67caf06930ed06e472686017929909
alireza kavian <kay.alireza@gmail.com>
2024-01-14 08:48:39 +0330

    melika reborn

--- blog/component---src-pages-contact-js-ad7b74f44e5e1af708ac.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"Cuy+":function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),c=t("TJpk"),u=t.n(c),i=t("83Zx"),o=t("IpnI"),r=t.n(o);n.default=function(){return l.a.createElement(i.a,null,l.a.createElement("main",null,l.a.createElement(u.a,{title:"Contact | "+r.a.siteTitle}),l.a.createElement("h1",null,"Contact us"),l.a.createElement("p",null,"Contact informations")))}}}]);
-//# sourceMappingURL=component---src-pages-contact-js-ad7b74f44e5e1af708ac.js.map
